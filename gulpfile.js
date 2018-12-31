c872cd8e1b0667af19aacdeb42c95faf475ecb98
Alex Clever <axclever@gmail.com>
2018-12-31 20:54:58 +0200

    added copyright

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,25 @@
-var gulp = require('gulp');
-var stylus = require('gulp-stylus');
-var concat = require('gulp-concat');
+const gulp = require('gulp');
+const stylus = require('gulp-stylus');
+const concat = require('gulp-concat');
 
 gulp.task('css', function () {
-    gulp.src('./src/css/*.styl')
+    return gulp.src('./src/css/*.styl')
         .pipe(stylus())
         .pipe(concat("styles.css"))
         .pipe(gulp.dest('./static/css/'));
 });
 
 gulp.task('js', function () {
-    gulp.src('./src/js/*.js')
+    return gulp.src('./src/js/*.js')
         .pipe(concat("main.js"))
         .pipe(gulp.dest('./static/js/'));
 });
 
 
-gulp.task('watch', function () {
-    gulp.watch(['./src/css/*.styl'], ['css']);
-    gulp.watch(['./src/css/*.js'], ['js']);
-});
+// gulp.task('watch', function () {
+//     gulp.parallel(gulp.watch(['./src/css/*.styl'], ['css']),
+//     gulp.watch(['./src/css/*.js'], ['js']));
+// });
 
 
-gulp.task("default", ['css', 'js', 'watch']);
+gulp.task("default", gulp.series(['css', 'js']));
